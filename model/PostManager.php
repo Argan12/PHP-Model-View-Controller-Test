649ce9d9eca1358e4aa1021da583e9fc51174ac4
Argan Piquet <43995661+Argan12@users.noreply.github.com>
2018-10-16 16:39:15 +0200

    Update PostManager.php

--- model/PostManager.php
+++ model/PostManager.php
@@ -2,6 +2,7 @@
 	require_once('model/Database.php');
 	
 	class PostManager extends Database {
+		// Retrieve posts in database
 		public function getPosts() {
 			$database = $this->dbConnect();
 			$searchPosts = $database->query('SELECT post_id, title, content, DATE_FORMAT(date, \'%d/%m/%Y à %H:%i\') AS datetime FROM mvctest ORDER BY post_id DESC');
@@ -9,6 +10,7 @@
 			return $searchPosts;
 		}
 		
+		// Get one specific post with its ID
 		public function getPost($postID) {
 			$database = $this->dbConnect();
 			$displayPost = $database->prepare('SELECT post_id, title, content, DATE_FORMAT(date, \'%d/%m/%Y à %H:%i\') AS datetime FROM mvctest WHERE post_id = ?');
@@ -17,7 +19,8 @@
 			
 			return $post;
 		}
-	
+		
+		// Save post in database
 		public function postEdit($title, $content) {
 			$database = $this->dbConnect();
 			$edit = $database->prepare('INSERT INTO mvctest(title, content, date) VALUES(?, ?, NOW())');
@@ -26,6 +29,7 @@
 			return $save;
 		}
 		
+		// Save new post content
 		public function postUpdate($postID, $title, $content) {
 			$database = $this->dbConnect();
 			$update = $database->prepare('UPDATE mvctest SET title = :title, content = :content WHERE post_id = :post_id');
@@ -38,6 +42,7 @@
 			return $update;
 		}
 		
+		// Delete post from database
 		public function postDelete($postID) {
 			$database = $this->dbConnect();
 			$infoPost = $database->prepare('DELETE FROM mvctest WHERE post_id = ?');
